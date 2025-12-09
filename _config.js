eba1eeb8df5c6bb13a48a3dce6a426a732208d5f
MaBoiteALocAngevine <maboitealocangevine@gmail.com>
2025-12-09 11:58:22 +0100

    Add files via upload

---
+++ _config.js
@@ -0,0 +1,19 @@
+﻿// --- _config.js ---
+// Variables globales et configuration
+
+// Panier (chargé depuis le localStorage si existant)
+const initialCart = JSON.parse(localStorage.getItem('panier') || '[]');
+let panier = initialCart;
+
+// Données des produits chargées depuis le CSV
+let allProductsData = []; 
+
+// État temporaire pour la modale
+let selectedProductForModal = null; 
+
+// Variables pour le carrousel
+let currentSlide = 0;
+let totalSlides = 0;
+let carouselInterval; 
+
+const CAROUSEL_INTERVAL_MS = 5000; // Intervalle de 5 secondes
